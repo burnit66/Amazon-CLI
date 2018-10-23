7b5f924eb3cc77f36a91f8cceb1730258894fb69
joe <burnett.joe@gmail.com>
2018-10-22 21:29:11 -0400

    creating customer interface

---
+++ bamazonCustomer.js
@@ -0,0 +1,108 @@
+var mysql = require("mysql");
+var cTable = require("console.table");
+var inquirer = require("inquirer");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 8889,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "root",
+
+    database: "bamazonDB"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Server is listening and connected" + "\n");
+    listProducts();
+    whatToDo();
+});
+
+function listProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.log("\n")
+        console.table(res);
+    });
+}
+
+function whatToDo() {
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+
+        inquirer.prompt([{
+                name: "choice",
+                type: "input",
+                message: "What is the product ID of the item you'd like to buy?",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: "How many would you like to buy?",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        ]).then(function (answer) {
+
+            var userChoice = Number(answer.choice)
+            var userQuantity = Number(answer.quantity)
+            var productReturn;
+
+            function checkStock() {
+                if (userChoice > results.length) {
+                    console.log("This is not a valid product!")
+                    return false
+                }
+                for (i = 0; i < results.length; i++) {
+                    if (userChoice === results[i].item_id) {
+                        console.log("You chose: " + results[i].product_name + "\n")
+                        productReturn = results[i]
+                        return true;
+                    }
+                }
+            }
+            checkStock()
+
+            function fulfillOrder(productReturn, userQuantity) {
+                if (userQuantity <= results[i].stock_quantity) {
+                    var newStock = productReturn.stock_quantity - userQuantity
+                    connection.query("UPDATE products SET ? WHERE ?",
+                        [{
+                                stock_quantity: newStock
+                            },
+                            {
+                                item_id: productReturn.item_id
+                            }
+                        ],
+                        function (err) {
+                            if (err) throw err
+                            console.log("Congratulations! You just ordered " + userQuantity + " " + productReturn.product_name + "(s)" + "\n")
+                            connection.end()
+                        })
+                    return true;
+                } else {
+                    console.log("Insufficient quantity! We only have " + productReturn.stock_quantity + " " + productReturn.product_name + "(s)" + "\n")
+                    return false
+                }
+            }
+            fulfillOrder(productReturn, userQuantity)
+        });
+    });
+}
--- bamazonDB.sql
+++ bamazonDB.sql
@@ -1,1 +1,26 @@
+-- RUN ONE
 CREATE DATABASE bamazonDB;
+
+
+-- RUN TWO
+USE bamazonDB;
+DROP TABLE products;
+CREATE TABLE products (
+    item_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(100) NOT NULL,
+    department_name VARCHAR(100),
+    price DECIMAL(10,2) NOT NULL,
+    stock_quantity INT(5) NOT NULL,
+    PRIMARY KEY (item_id)
+);
+
+
+-- RUN THREE
+USE bamazonDB;
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Towels", "Bathroom", 21.40, 4), ("Soap", "Bathroom", 4.99, 12), ("Bowls", "Kitchen", 4.20, 5), ("Oven Mitts", "Kitchen", 12.85, 2), ("Mixer", "Kitchen", 199.90, 3), ("Silverware set", "Kitchen", 25.99, 3), ("Bath Matt", "Bathroom", 14.99, 9), ("Shower Curtain", "Bathroom", 21.15, 2), ("Kitchen Table", "Kitchen", 220.00, 2), ("Rug", "Home", 64.99, 10), ("Egg Chair", "Home", 89.99, 5), ("Pot and pan set", "Kitchen", 249.99, 3), ("Keychains", "ETC", 0.99, 11), ("Gum", "ETC", 0.79, 13);
+
+
+--RUN FOUR
+USE bamazonDB;
+SELECT * FROM products;
